dbe33a744cc9eb208501c22d7547f6527d149369
tommytopas <topasdrum@gmail.com>
2015-05-06 15:34:55 +0200

    initial commit

---
+++ SortingAlgorithms/ISorter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithms
+{
+    abstract class ISorter
+    {
+        abstract public void Sort<T>(IList<T> list) where T : IComparable;
+
+        protected void Swap<T>(IList<T> list, int index1, int index2)
+        {
+            if (index1 == index2) return;
+            T temp = list[index1];
+            list[index1] = list[index2];
+            list[index2] = temp;
+        }
+    }
+}
