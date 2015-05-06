4a299fdb4fdbaec221ec61724910d21298ad8eae
tommytopas <topasdrum@gmail.com>
2015-05-06 15:35:35 +0200

    InsertionSort

---
+++ SortingAlgorithms/InsertionSort.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithms
+{
+    /// <summary>
+    /// Algorithm description: http://www.sorting-algorithms.com/insertion-sort
+    /// </summary>
+    class InsertionSort : ISorter
+    {
+        public override void Sort<T>(IList<T> list)
+        {
+            for (int i = 1; i < list.Count; i++)
+                for (int j = i; j > 0 && list[j].CompareTo(list[j - 1]) < 0; j--)
+                    Swap(list, j, j - 1);
+        }
+    }
+}
