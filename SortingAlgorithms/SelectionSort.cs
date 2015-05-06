b876236bd431956d06dbaa97cc486187b6296aff
tommytopas <topasdrum@gmail.com>
2015-05-06 15:35:49 +0200

    SelectionSort

---
+++ SortingAlgorithms/SelectionSort.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithms
+{
+    /// <summary>
+    /// Algorithm description: http://www.sorting-algorithms.com/selection-sort
+    /// </summary>
+    class SelectionSort : ISorter
+    {
+        public override void Sort<T>(IList<T> list)
+        {
+            for (int i = 0; i < list.Count - 1; i++)
+            {
+                int min = i;
+
+                for (int j = i + 1; j < list.Count; j++)
+                    if (list[j].CompareTo(list[min]) < 0)
+                        min = j;
+
+                Swap(list, i, min);
+            }
+        }
+    }
+}
